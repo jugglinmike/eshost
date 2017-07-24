4ec7468a0a8d00cf6d5be7201955b7ff9f476ae8
Mike Pennisi <mike@mikepennisi.com>
2017-07-24 19:36:07 -0400

    Maybe fix it

--- runtimes/browser.js
+++ runtimes/browser.js
@@ -1,14 +1,5 @@
 (function() {
 'use strict';
-var realmId = new Date().getTime();
-// Need to cache this reference because it looks like events are firing within
-// iframes after they have been detached from the DOM.
-var TOP = top;
-var log = window.log = function(msg) {
-  var p = TOP.document.createElement('p');
-  p.innerText = msg;
-  TOP.document.body.appendChild(p);
-};
 
 // The global $ binding will be removed if the `shortName` option is in use.
 // Maintain a function-scoped binding for internal use.
@@ -21,7 +12,6 @@ var $ = window.$ = {
   // character).
   shortName: '$ '[0],
   createRealm: function (options) {
-	log('createRealm 1');
     options = options || {};
     const globals = options.globals || {};
 
@@ -30,7 +20,6 @@ var $ = window.$ = {
     var fwin = frame.contentWindow;
     var fdoc = fwin.document;
 
-	log('createRealm 2');
     // The following is a workaround for a bug in Chromium related to reporting
     // errors produced from evaluating code using `eval`.
     // https://bugs.chromium.org/p/chromium/issues/detail?id=746564
@@ -38,7 +27,6 @@ var $ = window.$ = {
 
     var fscript = fdoc.createElement('script');
 
-	log('createRealm 3');
     fscript.textContent = this.source;
     fdoc.body.appendChild(fscript);
     var f$ = fwin.$;
@@ -47,15 +35,12 @@ var $ = window.$ = {
     f$.source = this.source;
     f$.socket = this.socket;
 
-	log('createRealm 4');
     for(var glob in globals) {
       fwin[glob] = globals[glob];
     }
 
     f$.destroy = function () {
-	  log('createRealm - destroy 1');
       document.body.removeChild(frame);
-	  log('createRealm - destroy 2');
 
       if (options.destroy) {
         options.destroy();
@@ -65,15 +50,12 @@ var $ = window.$ = {
     return f$;
   },
   evalScript: function (code, options) {
-	log('evalScript ' + realmId + ' ' + code);
     options = options || {};
 
     var s = document.createElement('script');
     s.textContent = code;
     var error = null;
-	log('evalScript 2');
-    window.onerror = function (msg, file, row, col, err) {
-	  log('onerror 1');
+    $.global.onerror = function (msg, file, row, col, err) {
       if (!err) {
         // make up some error for Edge.
         err = {
@@ -81,41 +63,15 @@ var $ = window.$ = {
           message: msg
         };
       }
-	  log('onerror 2: ' + err.message);
 
       error = err;
     }
-	log('evalScript 3');
-	try {
-	  log('evalScript 3.1' + s.textContent);
     document.body.appendChild(s);
-	  log('evalScript 3.2');
-	} catch (err) {
-	  log('evalScript 3.3');
-	  throw err;
-	}
-	log('evalScript 4');
-	try {
-	  window;
-	} catch (err) {
-	  log('evalScript 4.01' + err.name + ':' + err.message);
-	}
-	  log('evalScript 4.02');
-    if (window) {
-	  log('evalScript 4.1');
-	  try {
-      window.onerror = null;
-	    log('evalScript 4.2');
-	  } catch(err) {
-	    log('evalScript 4.3');
-	  }
-    }
+    $.global.onerror = null;
 
     if (error) {
-	  log('evalScript 5a');
       return { type: 'throw', value: error };
     } else {
-	  log('evalScript 5b');
       return { type: 'normal', value: undefined };
     }
   },
@@ -132,9 +88,7 @@ var $ = window.$ = {
 };
 
 function print(str) {
-  log('printing: ' + str);
   $.socket.emit('print', str);
-  log('printed!');
 }
 
 window.print = print;
